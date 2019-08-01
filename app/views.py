61f272cacbf05526af59403943aedc6a3253253b
gkarumba <gachegua@gmail.com>
2019-08-01 15:26:18 +0300

    Add the landing_page fn to the app/views.py

--- app/views.py
+++ app/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-
+from .forms import NewProjectForm,ReviewForm,UpdateProfileForm,RatemeSubscriptionForm
+from .email import send_welcome_email
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def landing_page(request):
+    projects = Projects.get_all_projects()
+    retur render(request,'index.html',{'projects':projects})
+    
