69e1adeb802579404c52fa9b8026a3757aa4bc48
gkarumba <gachegua@gmail.com>
2019-08-01 12:53:54 +0300

    Add the Profiles models to the app/models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,31 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profiles(models.Model):
+    image = models.ImageField(blank=True,)
+    bio = models.CharField(max_length=100)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    contact = models.IntegerField()
+    
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod
+    def get_profile_by_name(cls,name):
+        profile = Profiles.objects.filter(user__username__icontains = name)
+        return profile
+    
+    @classmethod
+    def filter_profile_by_id(cls,id):
+        profile = Profiles.objects.filter(user = id).first()
+        return profile
+    
+    @classmethod
+    def get_profile_by_id(cls,id):
+        profile = Profiles.objects.get(user = id)
+        return profile
+    
